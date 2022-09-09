7dc745c930907963705789ba61fe8af701943793
samuel aniekan <samuelaniekan680@gmail.com>
2022-09-09 07:26:21 +0100

    removed password validator

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -15,7 +15,8 @@ class RegisterSerializer(serializers.ModelSerializer):
     )
 
     password = serializers.CharField(
-        write_only=True, required=True, validators=[validate_password]
+        write_only=True,
+        required=True,  # validators=[validate_password]
     )
 
     ref_code = serializers.CharField(required=False)
